0a72451237f610efbb1f88351421842b64ffb0e6
Leandro Vinicius <leandrovinicius36@gmail.com>
2026-02-26 10:26:54 -0400

    refactor(api): implement @nestjs/config with zod validation

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import path from 'node:path';
+import { envSchema } from './common/configs/env.schema';
 import { UsersModule } from './users/users.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [UsersModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(__dirname, '../../../.env'),
+      validate: (config) => envSchema.parse(config),
+    }),
+    UsersModule,
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
--- apps/api/src/health/interfaces/http/health.controller.ts
+++ apps/api/src/health/interfaces/http/health.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 import {
   HealthCheck,
   HealthCheckService,
@@ -9,21 +10,18 @@ import {
 } from '@nestjs/terminus';
 import { PrismaService } from '../../../prisma/prisma.service';
 import { HealthResponseDto } from '../dto/health-response.dto';
+import { Env } from '../../../common/configs/env.schema';
 
 @ApiTags('health')
 @Controller('health')
 export class HealthController {
-  private readonly MEMORY_HEAP_THRESHOLD_BYTES =
-    Number(process.env.MEMORY_HEAP_THRESHOLD_BYTES) || 300 * 1024 * 1024; // 300MB
-  private readonly DISK_STORAGE_THRESHOLD_PERCENT =
-    Number(process.env.DISK_STORAGE_THRESHOLD_PERCENT) || 0.8; // 80%
-
   constructor(
     private health: HealthCheckService,
     private prisma: PrismaHealthIndicator,
     private db: PrismaService,
     private memory: MemoryHealthIndicator,
     private disk: DiskHealthIndicator,
+    private configService: ConfigService<Env, true>,
   ) {}
 
   @Get()
@@ -33,14 +31,22 @@ export class HealthController {
     type: HealthResponseDto,
   })
   check(): Promise<HealthResponseDto> {
+    const memoryHeapThreshold = this.configService.get(
+      'MEMORY_HEAP_THRESHOLD_BYTES',
+      { infer: true },
+    );
+    const diskStorageThreshold = this.configService.get(
+      'DISK_STORAGE_THRESHOLD_PERCENT',
+      { infer: true },
+    );
+
     return this.health.check([
       () => this.prisma.pingCheck('database', this.db.prisma),
-      () =>
-        this.memory.checkHeap('memory_heap', this.MEMORY_HEAP_THRESHOLD_BYTES),
+      () => this.memory.checkHeap('memory_heap', memoryHeapThreshold),
       () =>
         this.disk.checkStorage('storage', {
           path: '/',
-          thresholdPercent: this.DISK_STORAGE_THRESHOLD_PERCENT,
+          thresholdPercent: diskStorageThreshold,
         }),
     ]);
   }
