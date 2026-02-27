a00fe44b6086ad87dd23a6f68bcb9bb394d15b37
Rickson Lima <rickson.lima.dev@gmail.com>
2026-02-27 15:45:44 -0400

    refactor: remove domain-filter-exception and let nestjs handle errors

--- apps/api/src/users/domain/exceptions/user-not-found.exception.ts
+++ apps/api/src/users/domain/exceptions/user-not-found.exception.ts
@@ -1,6 +1,7 @@
-export class UserNotFoundException extends Error {
+import { NotFoundException } from '@nestjs/common';
+
+export class UserNotFoundException extends NotFoundException {
   constructor(id: string) {
     super(`User with id "${id}" not found`);
-    this.name = 'UserNotFoundException';
   }
 }
